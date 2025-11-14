82634d5c9ba60ade066a743663115ea3b1daef04
Aayush Bharda <aayushbharda999@gmail.com>
2025-11-14 19:02:07 +0530

    Configured frontend + backend for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
 import { Auth0Provider } from "@auth0/auth0-react";
 import { AuthProvider } from "./context/AuthContext";
-import { HashRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import { ChatProvider } from "./context/ChatContext";
+
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
