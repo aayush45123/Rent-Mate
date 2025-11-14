76fcb141202a41e18ded80b7038d6413241c7d2f
Aayush Bharda <aayushbharda999@gmail.com>
2025-11-14 18:42:05 +0530

    Configured frontend + backend for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/RentMate/",
+  base: "/Rent-Mate/", // <-- ADD THIS
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
